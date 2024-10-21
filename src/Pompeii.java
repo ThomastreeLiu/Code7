bee1700d9f697949fadbe949bba04425be756513
ThomastreeLiu <thomastree@126.com>
2024-10-20 21:57:54 -0700

    Update Pompeii.java

--- src/Pompeii.java
+++ src/Pompeii.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Pompeii {
    public static void main(String[] args){
-
+      String again = "y";
+      while (again.equals("y")){
+         
+      }
    }
-
 }
