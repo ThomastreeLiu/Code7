967645c08ae052fdd5f5c44fdb15268813084362
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-22 10:36:24 -0700

    Code7 half-complete(half-filled)

--- src/Pompeii.java
+++ src/Pompeii.java
@@ -25,7 +25,7 @@ public class Pompeii {
           } catch (InterruptedException e) {
               throw new RuntimeException(e);
           }
-          //sleep for 1000ms
+          //sleep for 5000ms
           System.out.println("Now we are going to start our trip, here is the map of pompeii.");
          for (int collections = 0;collections<6;) {
 
@@ -73,9 +73,12 @@ public class Pompeii {
             System.out.println("You have collect "+collections+" collections.");
             int location = scan.nextInt();
             if (location==1){
-               System.out.println("We are now at Forest park, this is ...");
+               System.out.println("Now we are in the Pompeii forest park which located at the north-east of the Pompeii city. It was one of the \n" +
+                       "favorite places of Pompeinans, who enjoyed relaxing walks here, and its wonderful and unique\n" +
+                       "environment inspire many poets. They left a lot of wonderful love on the walls of the streets of Pompeii.\n" +
+                       "You can find some cards in this forest park, there are poems on the cards. You can collect them.\n");
                if (forest==1){
-                  System.out.println("You got the collection Limb");
+                  System.out.println("You got the collection Poem card");
                   forest=0;
                   collections=collections+1;
                }
@@ -86,7 +89,11 @@ public class Pompeii {
                   collections = 11;
             }
             else if (location==2){
-               System.out.println("We are now at Temple of Venus, this is ...");
+               System.out.println("We are now at Temple of Venus, this was a place that the acient Pompeii people engaged in a series of \n" +
+                       "religious and ceremonial pratices to express their reverence and prayers to the goddess Venus.\n" +
+                       "People who lived in Pompeii also would hold weddings in this temple. They believe their weddings \n" +
+                       "could be blessed by the Venus. You can look for some shells in this temple and collect them, these shells\n" +
+                       "repersent Venus because the ocean wasm the place she borned.");
                if (temple ==1){
                   System.out.println("You got the collection Shell");
                   temple =0;
@@ -99,7 +106,11 @@ public class Pompeii {
             }
             //Temple
             else if (location==3){
-               System.out.println("We are now at Victory Square, this is ...");
+               System.out.println("I'd like to ask you to look up and see a tall tower in the front of you. That is the center of the \n" +
+                       " Ponpeii victory square, which is a square shape square. To the left of which are the farms, and at the end of Victory\n" +
+                       " Square is the theater. the leaders of Pompeii would give speeches here, and many important political\n" +
+                       " events would take place in this square.There are some badge hide in the corner in the victory square, \n" +
+                       " you can find them and collect them together.");
                if (square ==1){
                   System.out.println("You got the collection Winner's Badge");
                   square=0;
@@ -112,7 +123,11 @@ public class Pompeii {
             }
             //Square
             else if (location==4){
-               System.out.println("We are now at Star Theater, this is ...");
+               System.out.println("This expanse where we are now is the Star Theater, a theater that could hold over five hundred\n" +
+                       " people at a time, which was very large for that time.It was the largest theater in Pompeii and \n" +
+                       " was very popular with the people of Pompeii at that time. Many writers would do their best to \n" +
+                       " put their plays on this stage. There are many vintage ticket stubs in this theater with some \n" +
+                       " interesting information from that era, and you can collect them all!");
                if (theater==1){
                   System.out.println("You got the collection Ticket of Theater");
                   theater=0;
@@ -125,7 +140,10 @@ public class Pompeii {
             }
             //Theater
             else if (location==5){
-               System.out.println("We are now at Coin Farm, this is ...");
+               System.out.println("What you see in front of you is an open field some people would take for a parking lot, just \n" +
+                       " kidding. This is actually a farm in Pompeii. Many different crops were grown here, but grapes \n" +
+                       " and olives were the main ones, and Pompeii was famous for its wine and olive oil at that time!\n" +
+                       " You can collect some seeds if you want, enjoy your time here!!!");
                if (farm==1){
                   System.out.println("You got the collection A Package of Seeds");
                   farm=0;
@@ -138,7 +156,12 @@ public class Pompeii {
             }
             //Farm
             else if (location==6){
-               System.out.println("We are now at Living area, this is ...");
+               System.out.println("This is the most important place for the inhabitants of Pompeii - the residential area. \n" +
+                       " There are about 20,000 people living in Pompeii, and the residential area is well \n" +
+                       " planned and easily accessible, even the paths between the houses are well organized.\n" +
+                       " According to the research, archaeologists have found some fossilized eggs in this \n" +
+                       " area, and we have put some replicas in this area, so if you are interested, you \n" +
+                       " can look for them and collect them.");
                if (area==1){
                   System.out.println("You got the collection A Fossilized Egg");
                   area=0;
